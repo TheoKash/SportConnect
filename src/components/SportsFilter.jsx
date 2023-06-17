cef9d614ddfcd8ee2e565f0b89edc67937181d33
pv920 <petros.vasileiadis20@imperial.ac.uk>
2023-06-17 16:32:08 +0100

    Added select all sports option in sports filter.

--- src/components/SportsFilter.jsx
+++ src/components/SportsFilter.jsx
@@ -18,6 +18,32 @@ function SportsFilter(props) {
     		Sport
   		</button>
 			<ul class="dropdown-menu">
+				<li>
+					<input 
+						class="form-check-input" 
+						type="checkbox" 
+						id="all" 
+						style={{ marginRight: "10px", marginLeft: "10px" }} 
+						onChange={(e) => {
+							if (e.target.checked) {
+								props.setFilters({ ...props.filters, sports: props.sports })
+								// set all sports to checked
+								for (let sport of props.sports) {
+									document.getElementById(sport).checked = true;
+								}
+							} else {
+								props.setFilters({ ...props.filters, sports: new Set() })
+								// set all sports to unchecked
+								for (let sport of props.sports) {
+									document.getElementById(sport).checked = false;
+								}
+							}}
+						}
+						checked={props.filters.sports.size === props.sports.size}
+					/>
+					<label class="form-check-label" for="all"> Select All </label>
+				</li>
+				<li><hr class="dropdown-divider"/></li>
 				{[...props.sports].sort().map((sport) => (
 					<li>
 						<input 
@@ -26,6 +52,7 @@ function SportsFilter(props) {
 							id={sport} 
 							style={{ marginRight: "10px", marginLeft: "10px" }} 
 							onChange={handleCheckboxChange}
+							checked={props.filters.sports.has(sport)}
 						/>
 						<label class="form-check-label" for={sport}> {sport} </label>
 					</li>
